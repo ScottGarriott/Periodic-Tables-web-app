1e11264349ded98ee101742b32679a0d2e0f2d51
Scott Garriott <garriottscott@gmail.com>
2022-01-30 18:45:16 -0800

    fix cors options

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -11,10 +11,13 @@ const reservationsRouter = require("./reservations/reservations.router");
 const tablesRouter = require("./tables/tables.router")
 
 const app = express();
+var corsOptions = {
+    origin: 'https://git.heroku.com/periodic-tables-7447-front-end.git',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
 
-app.use(cors({origin: 
-    "https://git.heroku.com/periodic-tables-7447-front-end.git"
-}));
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/reservations", reservationsRouter);
